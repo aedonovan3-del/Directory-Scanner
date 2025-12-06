d6eeff612ae7e83248068c18157dce049264c3fe
D_ <aedonovan3@gmail.com>
2025-12-05 19:59:32 -0500

    Update run.py

--- run.py
+++ run.py
@@ -1,13 +1,16 @@
 #run
-
+import os
 from scanner import scan_directory
 from analyzer import analyze_files
 from scorer import score
 from report import write_csv_report
 
-def run():
-    root_path=r"C:\Users\aDonovan\Testing123\Tests"#substitute your own path
-
+def run(root_path):
+    """
+    Purpose: run the modules
+    Parameters: string for the path to the directory
+    Returns: writes the report of the potential automation candidates to a csv file
+    """    
     weights = [0.0,0.001,-0.1,1.0]#bias,size,days since mod,extension score
 
     files = scan_directory(root_path)
@@ -17,7 +20,12 @@ def run():
     #report
     output_csv = "automation_candidates.csv"
     write_csv_report(scored,output_csv)
-    print(f"report printed")
+    print(f"Report written to {os.path.abspath(output_csv)}")
 
 if __name__ == "__main__":
-    run()    
+    folder = input("Enter directory to scan (blank = current directory): ").strip()
+
+    if not folder:
+        folder = os.getcwd()
+
+    run(folder)
