e84e381e39695a7c0da518eb2a591f21c992a5e9
D_ <aedonovan3@gmail.com>
2025-12-05 19:40:09 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="directory_scanner",
+    version="0.1",
+    packages=find_packages(),
+    description="Minimal directory scanner that ranks files for automation using a perceptron-style score.",
+)
